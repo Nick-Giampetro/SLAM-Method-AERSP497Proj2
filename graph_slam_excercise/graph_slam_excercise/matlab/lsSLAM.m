f5694c55b1bdef610b2d905853ec13ede9a88405
Ankit <ankitg489@gmail.com>
2023-10-24 17:01:50 -0400

    complete code
    complete code

---
+++ graph_slam_excercise/graph_slam_excercise/matlab/lsSLAM.m
@@ -0,0 +1,62 @@
+more off;
+clear all;
+close all;
+
+addpath('tools');
+
+% load the graph into the variable g
+% only leave one line uncommented
+
+% simulation datasets
+%load ../data/simulation-pose-pose.mat
+%load ../data/simulation-pose-landmark.mat
+
+% real-world datasets
+%load ../data/intel.mat
+load ../data/dlr.mat
+
+% the number of iterations
+numIterations = 100;
+
+% maximum allowed dx
+EPSILON = 10^-4;
+
+% Error
+err = 0;
+
+% plot the initial state of the graph
+plot_graph(g, 0);
+
+fprintf('Initial error %f\n', compute_global_error(g));
+
+% iterate
+for i = 1:numIterations 
+  fprintf('Performing iteration %d\n', i);
+
+  % compute the incremental update dx of the state vector
+  dx = linearize_and_solve(g);
+
+
+  % (TODO) apply the solution to the state vector g.x
+    
+  g.x = g.x + dx;
+
+
+  % plot the current state of the graph
+  plot_graph(g, i);
+
+  % Compute the global error given the current graph configuration
+  err = compute_global_error(g);
+
+  % Print current error
+  fprintf('Current error %f\n', err);
+
+  % termination criterion  
+  if(max(abs(dx)) < EPSILON)
+	disp('Converged!!');
+	break;
+  end
+
+end
+
+fprintf('Done!\nFinal error %f\n', err);
